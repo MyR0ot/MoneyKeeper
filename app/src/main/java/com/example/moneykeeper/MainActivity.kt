7aaeaa5955b05014a221363a067b418620c0659a
unknown <myr00t@mail.ru>
2019-12-12 15:38:58 +0300

    setting design

--- app/src/main/java/com/example/moneykeeper/MainActivity.kt
+++ app/src/main/java/com/example/moneykeeper/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.moneykeeper
 
+import android.app.ActionBar
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -9,12 +10,18 @@ import com.balram.locker.utils.Locker
 import com.balram.locker.view.AppLocker
 import com.balram.locker.view.LockActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import android.view.animation.Animation
+import android.view.animation.Transformation
+
 
 class MainActivity : LockActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_main)
 
         btn_on_off.setOnClickListener(this) // TODO: kill this
@@ -22,6 +29,7 @@ class MainActivity : LockActivity(), View.OnClickListener {
         btn_change.setText(R.string.change_passcode)    // TODO: kill this
 
 
+        l_settings.visibility = View.GONE
         updateUI()
         loadRouting()
     }
@@ -64,7 +72,7 @@ class MainActivity : LockActivity(), View.OnClickListener {
 
 
     private fun updateUI(): Unit {
-        if(AppLocker.getInstance().appLock.isPasscodeSet){
+        if (AppLocker.getInstance().appLock.isPasscodeSet) {
             btn_on_off.setText(R.string.disable_passcode)
             btn_change.isEnabled = true
         } else {
@@ -87,6 +95,71 @@ class MainActivity : LockActivity(), View.OnClickListener {
             }
         }
 
+        btn_settings.setOnClickListener {
+            btn_settings.visibility = View.GONE
+            expand(l_settings)
+        }
+
+        tv_settings.setOnClickListener {
+            collapse(l_settings)
+            btn_settings.visibility = View.VISIBLE
+        }
+
+
+    }
+
+    private fun expand(v: View) {
+        val matchParentMeasureSpec =
+            View.MeasureSpec.makeMeasureSpec((v.parent as View).width, View.MeasureSpec.EXACTLY)
+        val wrapContentMeasureSpec =
+            View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+        v.measure(matchParentMeasureSpec, wrapContentMeasureSpec)
+        val targetHeight = v.measuredHeight
+
+        // Older versions of android (pre API 21) cancel animations for views with a height of 0.
+        v.layoutParams.height = 1
+        v.visibility = View.VISIBLE
+        val a = object : Animation() {
+            override fun applyTransformation(interpolatedTime: Float, t: Transformation) {
+                v.layoutParams.height = if (interpolatedTime == 1f)
+                    ActionBar.LayoutParams.WRAP_CONTENT // TODO: возможно заимпорптил не тот клас, не все норм!
+                else
+                    (targetHeight * interpolatedTime).toInt()
+                v.requestLayout()
+            }
+
+            override fun willChangeBounds(): Boolean {
+                return true
+            }
+        }
+
+        // Expansion speed of 1dp/ms
+        a.duration = (targetHeight / v.context.resources.displayMetrics.density).toInt().toLong()
+        v.startAnimation(a)
+    }
+
+    private fun collapse(v: View) {
+        val initialHeight = v.measuredHeight
+
+        val a = object : Animation() {
+            override fun applyTransformation(interpolatedTime: Float, t: Transformation) {
+                if (interpolatedTime == 1f) {
+                    v.visibility = View.GONE
+                } else {
+                    v.layoutParams.height =
+                        initialHeight - (initialHeight * interpolatedTime).toInt()
+                    v.requestLayout()
+                }
+            }
+
+            override fun willChangeBounds(): Boolean {
+                return true
+            }
+        }
+
+        // Collapse speed of 1dp/ms
+        a.duration = (initialHeight / v.context.resources.displayMetrics.density).toInt().toLong()
+        v.startAnimation(a)
     }
 
 
